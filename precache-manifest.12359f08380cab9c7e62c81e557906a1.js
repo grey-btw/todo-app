91697f13105c53febf525ff8d872cc5dadebadb2
grey-btw <lehoangtrung2211@gmail.com>
2020-10-14 23:53:07 +0700

    Updates

---
+++ precache-manifest.12359f08380cab9c7e62c81e557906a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdb83aa599f45c85286c7edf2f7e9f5",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "c7c54770461d53017a9f",
+    "url": "/todo-app/static/css/main.09e4f176.chunk.css"
+  },
+  {
+    "revision": "02e4c7dcc3fca687fec0",
+    "url": "/todo-app/static/js/2.96b47a17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.96b47a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c54770461d53017a9f",
+    "url": "/todo-app/static/js/main.b0908950.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
